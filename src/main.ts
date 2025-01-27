b05742a71c769697ca4b411165614b53d172d7f7
Essty <info@gotip.ru>
2025-01-27 19:48:13 +0100

    added basic structure

--- src/main.ts
+++ src/main.ts
@@ -1,31 +1,16 @@
 import { NestFactory } from '@nestjs/core';
-import * as dotenv from 'dotenv';
 import { AppModule } from './app.module';
-import {INestApplication, ValidationPipe} from "@nestjs/common";
-import { VercelRequest, VercelResponse } from '@vercel/node';
 
-let cachedApp: INestApplication | null = null;
-
-async function bootstrap(): Promise<INestApplication>  {
+async function bootstrap() {
   const origins = ['http://localhost:3001', 'http://localhost:3000'];
-  dotenv.config();
-  const app = await NestFactory.create(AppModule, { cors: true  });
+
+  const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: '*',//origins,
+    origin: origins,
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
   });
-  app.setGlobalPrefix('api');
-  app.useGlobalPipes(new ValidationPipe());
-  await app.init();
-  //await app.listen(5000);
-  return app;
+
+  await app.listen(5000);
 }
-//bootstrap();
-export default async function handler(req: VercelRequest, res: VercelResponse) {
-  if (!cachedApp) {
-    cachedApp = await bootstrap();
-  }
-  const expressApp = cachedApp?.getHttpAdapter().getInstance();
-  return expressApp(req, res);
-}
+bootstrap();
