c9c62dd1a18f50fb668cf4daa7f2f9e2fbbf3be9
Essty <info@gotip.ru>
2025-01-30 23:41:05 +0100

    update functions; clean up

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import {ValidationPipe} from "@nestjs/common";
 
 async function bootstrap() {
   const origins = ['http://localhost:3001', 'http://localhost:3000'];
 
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { cors: true  });
   app.enableCors({
     origin: origins,
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
   });
+  app.setGlobalPrefix('api');
+  app.useGlobalPipes(new ValidationPipe());
 
   await app.listen(5000);
 }
